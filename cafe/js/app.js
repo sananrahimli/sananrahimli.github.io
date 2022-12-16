8a8c89dd2172be2e1eccb2304a8a6cb3de10467f
Sanan <srweb13@gmail.com>
2022-12-16 18:00:50 +0400

    cf2

---
+++ cafe/js/app.js
@@ -0,0 +1,14 @@
+      var swiper = new Swiper(".cyberpunkSananRagimli", {
+        direction: "vertical",
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        slidesPerView: 1,
+        spaceBetween: 0,
+        mousewheel: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
